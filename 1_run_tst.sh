2d44603e55f19deac1d73da99cefcf9251b6c9a9
Victor Suarez <suarez.a.victor@gmail.com>
2026-03-28 19:14:38 -0400

    need to test on cluster but want to develop on local 3

--- 1_run_tst.sh
+++ 1_run_tst.sh
@@ -8,17 +8,17 @@ a_list2=(0.1 0.1 0.2 0.3 0.4 0.6 1.0 1.0 1.0 1.0 1.0 1.0 1.0)
 
 leps_list=(0.00 4.00 8.00 12.00 16.00)
 
-####################################
-### System A gamma/(M*omega_c)=1 ###
-####################################
-mkdir _sys_A_gam_1
-cd _sys_A_gam_1
+################
+### System A ###
+################
+mkdir _sys_A
+cd _sys_A
 
 ### Adiabatic BO rates ###
 mkdir adiabatic
 cd adiabatic
 sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=1 --fix=s --gam=1.0 --logK=-7.00 #g" ../../submit_template.slm
-#sbatch ../../submit_template.slm
+sbatch ../../submit_template.slm
 for logK in "${logK_list1[@]}"; do
   sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=1 --fix=s --gam=1.0 --logK=$logK #g" ../../submit_template.slm
   sbatch ../../submit_template.slm
@@ -57,99 +57,155 @@ cd ../
 ### New KC-RPMD rates ###
 
 cd ../
+################
+### System A ###
+################
+
+####################################
+### System A gamma/(M*omega_c)=1 ###
+####################################
+mkdir _sys_A_gam_1
+cd _sys_A_gam_1
+
+### Adiabatic BO rates ###
+mkdir adiabatic
+cd adiabatic
+sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=1 --fix=s --gam=1.0 --logK=-7.00 #g" ../../submit_template.slm
+sbatch ../../submit_template.slm
+for logK in "${logK_list2[@]}"; do
+  sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=1 --fix=s --gam=1.0 --logK=$logK #g" ../../submit_template.slm
+  sbatch ../../submit_template.slm
+done
+cd ../
+### Adiabatic BO rates ###
+
+### New KC-RPMD rates ###
+mkdir kcrpmd_new
+cd kcrpmd_new
+for fix in s y; do
+  for logK in "${logK_list2[@]}"; do
+    sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=3 --fix=$fix --a=0.1 --gam=1.0 --logK=$logK #g" ../../submit_template.slm
+    sbatch ../../submit_template.slm
+  done
+done
+cd ../
+### New KC-RPMD rates ###
+
+cd ../
 ####################################
 ### System A gamma/(M*omega_c)=1 ###
 ####################################
 
-exit 0
-
+######################################
+### System A gamma/(M*omega_c)=250 ###
+######################################
 mkdir _sys_A_gam_250
+cd _sys_A_gam_250
+
+### Adiabatic BO rates ###
+mkdir adiabatic
+cd adiabatic
+sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=1 --fix=s --gam=250.0 --logK=-7.00 #g" ../../submit_template.slm
+sbatch ../../submit_template.slm
+for logK in "${logK_list2[@]}"; do
+  sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=1 --fix=s --gam=250.0 --logK=$logK #g" ../../submit_template.slm
+  sbatch ../../submit_template.slm
+done
+cd ../
+### Adiabatic BO rates ###
+
+### New KC-RPMD rates ###
+mkdir kcrpmd_new
+cd kcrpmd_new
+for fix in s y; do
+  for logK in "${logK_list2[@]}"; do
+    sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=1 --meth=3 --fix=$fix --a=0.1 --gam=250.0 --logK=$logK #g" ../../submit_template.slm
+    sbatch ../../submit_template.slm
+  done
+done
+cd ../
+### New KC-RPMD rates ###
+
+cd ../
+######################################
+### System A gamma/(M*omega_c)=250 ###
+######################################
+
+################
+### System B ###
+################
 mkdir _sys_B
+cd _sys_B
+
+### Adiabatic BO rates ###
+mkdir adiabatic
+cd adiabatic
+sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=2 --meth=1 --fix=s --gam=1.0 --logK=-7.00 #g" ../../submit_template.slm
+sbatch ../../submit_template.slm
+for logK in "${logK_list1[@]}"; do
+  sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=2 --meth=1 --fix=s --gam=1.0 --logK=$logK #g" ../../submit_template.slm
+  sbatch ../../submit_template.slm
+done
+cd ../
+### Adiabatic BO rates ###
+
+### New KC-RPMD rates ###
+mkdir kcrpmd_new
+cd kcrpmd_new
+for fix in s y; do
+  for logK in "${logK_list1[@]}"; do
+    sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=2 --meth=3 --fix=$fix --a=0.1 --gam=1.0 --logK=$logK #g" ../../submit_template.slm
+    sbatch ../../submit_template.slm
+  done
+done
+cd ../
+### New KC-RPMD rates ###
+
+cd ../
+################
+### System B ###
+################
+
+################
+### System C ###
+################
 mkdir _sys_C
+cd _sys_C
 
-# System A high friction BO rate at K=0
-sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=1 --gam=32 --method=1 --fix=s --K0=1e-10 /g" submit_template.slm
-sbatch submit_template.slm
-
-# System A high friction BO rates
-for K0 in "${K0_list[@]}"; do
-  sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=1 --gam=32 --method=1 --fix=s --K0=$K0 /g" submit_template.slm
-  sbatch submit_template.slm
-done
-
-# System A high friction original KC-RPMD rates
-for fix in s y; do
-  for i in "${!K0_list[@]}"; do
-    K0=${K0_list[i]}
-    a=${a_list[i]}
-    sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=1 --gam=32 --method=2 --fix=$fix --a=$a --K0=$K0 /g" submit_template.slm
-    sbatch submit_template.slm
+### Adiabatic BO rates ###
+mkdir adiabatic
+cd adiabatic
+for leps in "${leps_list[@]}"; do
+  for hw in 1 -1; do
+    sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=3 --meth=1 --fix=s --gam=1.0 --logK=-7.00 --leps=$leps --hw=$hw #g" ../../submit_template.slm
+    sbatch ../../submit_template.slm
+    sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=3 --meth=1 --fix=s --gam=1.0 --logK=0.50 --leps=$leps --hw=$hw #g" ../../submit_template.slm
+    sbatch ../../submit_template.slm
   done
 done
+cd ../
+### Adiabatic BO rates ###
 
-# System A high friction original KC-RPMD rates set to a=0.1
-for fix in s y; do
-  for i in "${!K0_list[@]}"; do
-    K0=${K0_list[i]}
-    if [[ $i -lt 3 ]]; then
-      echo $K0
-      continue
+### New KC-RPMD rates ###
+mkdir kcrpmd_new
+cd kcrpmd_new
+for leps in "${leps_list[@]}"; do
+  for hw in 1 -1; do
+    if [[ "$hw" -= "1" ]]; then
+      sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=3 --meth=3 --fix=s --gam=1.0 --logK=0.50 --leps=$leps --hw=$hw #g" ../../submit_template.slm
+      sbatch ../../submit_template.slm
     fi
-    sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=1 --gam=32 --method=2 --fix=$fix --K0=$K0 /g" submit_template.slm
-    sbatch submit_template.slm
+    if [[ "$hw" -= "-1" ]]; then
+      sed -i "s#python.*#python ../../1_kcrpmd_tst.py --sys=3 --meth=3 --fix=y --gam=1.0 --logK=0.50 --leps=$leps --hw=$hw #g" ../../submit_template.slm
+      sbatch ../../submit_template.slm
+    fi
   done
 done
+cd ../
+### New KC-RPMD rates ###
 
-# System A high friction new KC-RPMD rates
-for fix in s y; do
-  for K0 in "${K0_list[@]}"; do
-    sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=1 --gam=32 --method=3 --fix=$fix --K0=$K0 /g" submit_template.slm
-    sbatch submit_template.slm
-  done
-done
+cd ../
+################
+### System C ###
+################
 
-# System B BO rate at K=0
-sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=2 --method=1 --fix=s --K0=1e-10 /g" submit_template.slm
-sbatch submit_template.slm
-
-# System B BO rates
-for K0 in "${K0_list[@]}"; do
-  sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=2 --method=1 --fix=s --K0=$K0 /g" submit_template.slm
-  sbatch submit_template.slm
-done
-
-# System B new KC-RPMD rates
-for fix in s y; do
-  for K0 in "${K0_list[@]}"; do
-    sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=2 --method=3 --fix=$fix --K0=$K0 /g" submit_template.slm
-    sbatch submit_template.slm
-  done
-done
-
-# System C BO rate at K=0 w/ adiabatic and non-adiabatic hard wall rates
-for leps in "${leps_list[@]}"; do
-  for hw in 1 -1; do
-    sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=3 --method=1 --fix=s --K0=1e-10 --leps=$leps --hw=$hw /g" submit_template.slm
-    sbatch submit_template.slm
-  done
-done
-
-# System C BO adiabatic and non-adiabatic hard wall rates
-for leps in "${leps_list[@]}"; do
-  for hw in 1 -1; do
-    sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=3 --method=1 --fix=s --leps=$leps --hw=$hw /g" submit_template.slm
-    sbatch submit_template.slm
-  done
-done
-
-# System C KC-RPMD adiabatic hard wall rates
-for leps in "${leps_list[@]}"; do
-  sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=3 --method=3 --fix=s --leps=$leps --hw=1 /g" submit_template.slm
-  sbatch submit_template.slm
-done
-
-# System C KC-RPMD nonadiabatic hard wall rates
-for leps in "${leps_list[@]}"; do
-  sed -i "s/python.*/python 1_kcrpmd_tst.py --sys=3 --method=3 --fix=y --leps=$leps --hw=-1 /g" submit_template.slm
-  sbatch submit_template.slm
-done
