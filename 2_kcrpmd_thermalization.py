5d0e1313d2bde375c6478d6afa2c91e1798c79cd
Victor Suarez <suarez.a.victor@gmail.com>
2026-03-28 19:35:26 -0400

    need to test on cluster but want to develop on local 5

--- 2_kcrpmd_thermalization.py
+++ 2_kcrpmd_thermalization.py
@@ -12,19 +12,10 @@ from liblibra_core import *
 import libra_py.dynamics.tsh.compute as tsh_dynamics
 
 # Add the parent directory to sys.path
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, parent_dir)
+#parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
+#sys.path.insert(0, parent_dir)
 
 from kcrpmd_utils.kcrpmdmodel import kcrpmd_system_bath
-
-# check current directory name
-current_dir = os.path.basename(os.getcwd())
-if current_dir.startswith("_sys_"):
-    idx = current_dir.find("_fix_")
-    fix = current_dir[idx + len("_fix_")]
-else:
-    print("not in correct directory, directory should start with '_sys_'.")
-    exit()
 
 # ======= READ IN KC-RPMD RECIPE, MODEL, AND INITIAL CONDITIONS =======
 
